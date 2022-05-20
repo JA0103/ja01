15d448da33d700854fc265b17131cc7b6241c7c5
JA <jinaa0103@gmail.com>
2022-05-20 17:52:55 +0900

    220520SQL

--- HR.sql
+++ HR.sql
@@ -609,5 +609,105 @@ from EMPLOYEES;
 
 
 
+select * from employees;
+select * from departments;
 
+
+
+  /*카티션 곱 : 모든 행에 대해서 조인*/
+select * from employees, departments;
+
+/*동등조인 : 조인하는 테이블에서 조인 조건이 일치하는 것만 조회  >>>  내부조인 */
+select * 
+from jobs, JOB_HISTORY
+where jobs.JOB_ID = JOB_HISTORY.JOB_ID;
+
+select C.region_id, L.location_id, C.country_id
+from countries C, locations L 
+where C.country_id = L.country_id;
+
+select * 
+from EMPLOYEES e , DEPARTMENTS d
+where e.DEPARTMENT_ID = d.DEPARTMENT_ID;
+
+select * from EMPLOYEES;
+select * from jobs;
+
+select e.SALARY, j.MIN_SALARY, j.MAX_SALARY
+from EMPLOYEES e, jobs j
+where e.salary BETWEEN j.min_salary and j.max_salary;
+
+select e.first_name, e.HIRE_DATE, j.START_DATE, j.END_DATE
+from employees e, job_history j
+where e.hire_date between j.start_date and j.end_date;
+
+select count(*)
+from employees
+where hire_date between '20070101' and '20071231';
+
+/*
+외부조인 : 조인하는 테이블엥서 조인 조건을 만족하지 않는 행도 출력
+        동등조인 조건을 만족하지 못하고 누락된 행을 출력할 때 사용
+*/
+
+select *
+from jobs j, JOB_HISTORY h
+where j.JOB_ID = h.JOB_ID (+) ;
+
+select *
+from jobs j, JOB_HISTORY h
+where j.JOB_ID(+) = h.JOB_ID;
+
+
+select *
+from COUNTRIES c, LOCATIONS l
+where c.COUNTRY_ID(+) = l.COUNTRY_ID;
+
+select * from EMPLOYEES;
+
+
+select a.FIRST_NAME, a.LAST_NAME, b.FIRST_NAME, b.LAST_NAME
+from EMPLOYEES a, EMPLOYEES b
+where a.manager_id = b.EMPLOYEE_ID;
+-- -> a상관 = b name
+
+
+/*
+countries 테이블과 locations 테이블을 country_id를 기준으로
+조인하여 country_namae, state_province, street_address를 조회
+*/
+select C.COUNTRY_NAME, L.state_province, L.street_address
+from COUNTRIES C, LOCATIONS L
+where C.COUNTRY_ID = L.COUNTRY_ID;
+
+select * from COUNTRIES;
+
+select * from LOCATIONS;
+
+/*
+jobs 테이블과 job_history 테이블을 job_id를 기준으로 조인하여 
+job_id, job_title, start_date, end_date를 조회
+*/
+select j.job_id, j.job_title, h.start_date, h.end_date
+from JOBS J , JOB_HISTORY H
+where J.JOB_ID = H.JOB_ID;
+
+/*
+employees 테이블과 departments 테이블을 department_id
+기준으로 조인하고, employees 테이블과 jobs 테이블을 job_id
+기준으로 조인하여 first_name, last_name, department_name, job_title을 조회
+*/
+select e.first_name, e.last_name, d.department_name, j.job_title
+from employees E, departments D, jobs J
+where E.DEPARTMENT_ID = D.DEPARTMENT_ID and E.JOB_ID = J.JOB_ID;
+
+/*
+countries 테이블과 locations 테이블을 locations의 
+country_id를 기준으로 외부조인하여 country_id, country_name,city를 조회
+*/
+select C.country_id, C.country_name,L.CITY
+from countries C, locations L
+where L.COUNTRY_ID(+) = C.COUNTRY_ID;
+
+select *from COUNTRIES;
 
